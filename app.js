6f0a0872f0d282a128cb2b56449f19ca95ddc72f
Ryan <ryansateles@gmail.com>
2021-11-08 13:49:23 -0300

    modifiquei os 3 botoes ataves do comp da leticia

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const button = document.querySelector('button');
 const h1 = document.querySelector('h1');
+const answerButtons = document.querySelectorAll('.square');
 
 let colors = { red: 0, green: 0, blue: 0 };
 
@@ -23,6 +24,22 @@ button.addEventListener('mouseover', () => {
     button.style.borderColor = newColor;
     button.style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
 })
+
+for (let i = 0; i < 3; i++) {
+    answerButtons[i].addEventListener('click', () => {
+        answerButtons[i].style.borderColor = newColor;
+        answerButtons[i].style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
+        answerButtons[i].addEventListener('mouseleave', () => {
+            answerButtons[i].style.borderColor = 'black';
+            answerButtons[i].style.boxShadow = `none`;
+        })
+    })
+
+    answerButtons[i].addEventListener('mouseover', () => {
+        answerButtons[i].style.borderColor = newColor;
+        answerButtons[i].style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
+    })
+}
 
 const randomColor = () => {
     colors.red = Math.floor(Math.random() * 255);
