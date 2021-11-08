a810583f8378363d1d62eedf094bd1db3966d368
Ryan Souza <ryansateles@gmail.com>
2021-11-08 15:17:02 -0300

    refatorada no codigo javascript

--- app.js
+++ app.js
@@ -7,39 +7,47 @@ let colors = { red: 0, green: 0, blue: 0 };
 button.addEventListener('click', () => {
     newColor = randomColor();
     const color = document.querySelector('#color');
+    button.style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
     color.style.backgroundColor = newColor;
     h1.innerText = `RGB(${colors.red}, ${colors.green}, ${colors.blue})`;;
-})
-
-button.addEventListener('click', () => {
     button.style.borderColor = newColor;
-    button.style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
-    button.addEventListener('mouseleave', () => {
-        button.style.borderColor = 'black';
-        button.style.boxShadow = `none`;
+
+    button.addEventListener('mouseover', () => {
+        button.style.borderColor = newColor;
+        button.style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
     })
+
+    for (let i = 0; i < 3; i++) {
+        answerButtons[i].addEventListener('mouseover', () => {
+            answerButtons[i].style.borderColor = newColor;
+            answerButtons[i].style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
+        })
+    }
 })
 
 button.addEventListener('mouseover', () => {
-    button.style.borderColor = newColor;
-    button.style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
+    button.style.borderColor = 'gray';
+    button.style.boxShadow = `0 0.8em 0.8em -0.4em black`;
+})
+
+button.addEventListener('mouseleave', () => {
+    button.style.borderColor = 'black';
+    button.style.boxShadow = `none`;
 })
 
 for (let i = 0; i < 3; i++) {
-    answerButtons[i].addEventListener('click', () => {
-        answerButtons[i].style.borderColor = newColor;
-        answerButtons[i].style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
-        answerButtons[i].addEventListener('mouseleave', () => {
-            answerButtons[i].style.borderColor = 'black';
-            answerButtons[i].style.boxShadow = `none`;
-        })
+    answerButtons[i].addEventListener('mouseover', () => {
+        answerButtons[i].style.borderColor = 'gray';
+        answerButtons[i].style.boxShadow = `0 0.8em 0.8em -0.4em black`;
     })
 
-    answerButtons[i].addEventListener('mouseover', () => {
-        answerButtons[i].style.borderColor = newColor;
-        answerButtons[i].style.boxShadow = `0 0.8em 0.8em -0.4em ${newColor}`;
+    answerButtons[i].addEventListener('mouseleave', () => {
+        answerButtons[i].style.borderColor = 'black';
+        answerButtons[i].style.boxShadow = `none`;
     })
 }
+
+
 
 const randomColor = () => {
     colors.red = Math.floor(Math.random() * 255);
