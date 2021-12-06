d82bf426940a447194284b30c273bb83ce70387c
Ryan Souza <ryansateles@gmail.com>
2021-12-06 17:00:02 -0300

    Alterações finais, adicionei um menu collapse no "Como jogar" e "Oq é RGB"

--- app.js
+++ app.js
@@ -4,59 +4,46 @@ const answerButtons = document.querySelectorAll('.square');
 const Nums = document.querySelectorAll('.square span');
 const background = document.querySelector('#color');
 const message = document.querySelector('#message');
+const collapse = document.querySelectorAll(".collapse");
 
 //Game variables
 let answer;
 let tries = 0;
 let playColor;
 let hiddenColorIndex = -1;
+let hiddenRgbIndex = -1;
 
-const dark_mode = document.querySelector(".checkbox")
-dark_mode.addEventListener('change', () => {
-    document.body.classList.toggle('dark');
-})
-
+//The Game
 let colors = { red: 0, green: 0, blue: 0 };
 
-const gamePromise = new Promise(r => r());
+const gamePromise = new Promise(start => start());
 
 button.addEventListener('click', () => {
-    gamePromise.then(() => startGame());
+    gamePromise.then(startGame);
 })
 
 function startGame() {
-    console.log("Game is on",);
     setInitialValue();
     startGameVisual();
     setButtonsValues();
     setButtonsEvents();
-    console.log("DEBUG: ");
-    console.log("playColor", playColor);
-    console.log("tries",tries);
 }
 
 function setButtonsEvents() {
-    answerButtons.forEach(b => {
-
-        b.addEventListener('mouseover', () => {
-            b.style.borderColor = this.playColor;
-            b.style.boxShadow = `0 0.8em 0.8em -0.4em ${this.playColor}`;
-        })
-
-        b.onclick = () => {
-            if (b.textContent == this.answer) {
-                gamePromise.then(() => endGame());
+    answerButtons.forEach(btn => {
+        btn.onclick = () => {
+            if (btn.textContent == this.answer) {
+                gamePromise.then(endGame);
             }
             else {
                 this.tries++;
                 if (this.tries == 2) {
-                    message.innerText = 'Agora ficou fácil, né!?'
+                    message.innerHTML = 'Agora ficou fácil, né!?'
                 } else {
                     message.innerText = 'Quase! Tente de novo'
                 }
-                b.style.visibility = 'hidden';
+                btn.style.visibility = 'hidden';
             }
-            console.log("After play",this.tries);
         }
     })
 }
@@ -64,74 +51,70 @@ function setButtonsEvents() {
 function setInitialValue() {
     this.tries = 0;
     this.playColor = getRandomColor();
+    this.hiddenRgbIndex = random3();
     this.hiddenColorIndex = random3();
     this.answer = getCorrectNum();
-    console.log("Index", this.hiddenColorIndex);
 }
 
 function endGame() {
     RGB_h1.innerText = 'Jogar novamente?';
     button.innerText = 'Claro!'
     message.innerText = 'Parabéns, você acertou!'
-    answerButtons.forEach(b => {
-        b.style.visibility = 'visible';
-        b.borderColor = 'black';
-        b.boxShadow = `none`;
-        b.disabled = true;
-        b.innerHTML = '<i class="fas fa-check"></i>';
-        b.onclick = null;
+    answerButtons.forEach(btn => {
+        btn.style.visibility = 'visible';
+        btn.borderColor = 'black';
+        btn.boxShadow = `none`;
+        btn.disabled = true;
+        btn.innerHTML = '<i class="fas fa-check"></i>';
+        btn.onclick = null;
     })
     button.disabled = false;
-    console.log("Game is over!");
 }
 
-
 function startGameVisual() {
-    background.style.backgroundColor = playColor;
-    RGB_h1.innerText = getRGBText();
+    background.style.backgroundColor = this.playColor;
+    RGB_h1.innerHTML = getRGBText();
     message.innerText = 'Qual número está mais próximo??';
 
     button.innerText = 'Boa sorte!'
     button.disabled = true;
-    button.style.borderColor = playColor;
-    button.style.boxShadow = `0 0.8em 0.8em -0.4em ${playColor}`;
-
-    answerButtons.forEach(b => {
-        b.style.borderColor = 'black';
-        b.style.boxShadow = `none`;
-        b.style.visibility = 'visible';
-    });
+    button.style.borderColor = this.playColor;
+    button.style.boxShadow = `0 0.8em 0.8em -0.4em ${this.playColor}`;
+    for (let btn of [button, ...answerButtons]) {
+        btn.addEventListener('mouseover', () => {
+            btn.style.borderColor = this.playColor;
+            btn.style.boxShadow = `0 0.8em 0.8em -0.4em ${this.playColor}`;
+        });
+    };
 }
 
 function setButtonsValues() {
-    console.log("Hidden after", this.hiddenColorIndex);
-    answerButtons.forEach((b, i) => {
-        b.disabled = false;
+    answerButtons.forEach((btn, i) => {
+        btn.disabled = false;
         if (i === this.hiddenColorIndex)
-            b.textContent = this.answer;
+            btn.textContent = this.answer;
         else
-            b.textContent = Math.floor(Math.random() * 255);
+            btn.textContent = Math.floor(Math.random() * 255);
     })
 }
 
 function random3() {
-    console.log("Random called!")
     return Math.round(Math.random() * 2);
 }
 
 function getRGBText() {
-    if (hiddenColorIndex === 0)
-        return `RGB(???, ${colors.green}, ${colors.blue})`
-    else if (hiddenColorIndex === 1)
-        return `RGB(${colors.red}, ???, ${colors.blue})`
+    if (this.hiddenRgbIndex === 0)
+        return `RGB(<span>???</span>, ${colors.green}, ${colors.blue})`
+    else if (this.hiddenRgbIndex === 1)
+        return `RGB(${colors.red}, <span>???</span>, ${colors.blue})`
     else
-        return `RGB(${colors.red}, ${colors.green}, ???)`
+        return `RGB(${colors.red}, ${colors.green}, <span>???</span>)`
 }
 
 function getCorrectNum() {
-    if (hiddenColorIndex === 0)
+    if (this.hiddenRgbIndex === 0)
         return colors.red;
-    else if (hiddenColorIndex === 1)
+    else if (this.hiddenRgbIndex === 1)
         return colors.green
     else
         return colors.blue
@@ -141,8 +124,26 @@ function getRandomColor() {
     colors.red = Math.floor(Math.random() * 255);
     colors.green = Math.floor(Math.random() * 255);
     colors.blue = Math.floor(Math.random() * 255);
-    playColor = `rgb(${colors.red}, ${colors.green}, ${colors.blue})`;
+    return `rgb(${colors.red}, ${colors.green}, ${colors.blue})`;
 }
+//Game not start
+
+
+//Effects 
+const dark_mode = document.querySelector(".checkbox")
+dark_mode.addEventListener('change', () => {
+    document.body.classList.toggle('dark');
+    collapse.forEach(icon => {
+        icon.classList.toggle("collapse-dark-mode");
+    })
+})
+
+collapse.forEach(question => {
+    question.addEventListener("click", () => {
+        question.classList.toggle("collapse-clicked");
+    })
+})
+
 
 for (let btn of [button, ...answerButtons]) {
     btn.addEventListener('mouseover', () => {
@@ -154,5 +155,3 @@ for (let btn of [button, ...answerButtons]) {
         btn.style.boxShadow = `none`;
     })
 }
-
-
